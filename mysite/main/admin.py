e8dac36ff4ea6a3467cba15c00c89c4eea9fcadf
Ikrom180 <ikrom6617@icloud.com>
2026-08-09 15:46:25 +0500

    Admin ended

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.templatetags.auth import register
+from .models import ToDoList, Item
 
 # Register your models here.
+admin.site.register(Item)
+admin.site.register(ToDoList)
